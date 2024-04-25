cc633b59900a34f00c16c0c25fad64a797229a6e
Abdalla Ahmed <abdalla.ahly@gmail.com>
2024-04-25 22:37:19 +0200

    Update User.cs

--- LogisticsDataCore/Tables/User.cs
+++ LogisticsDataCore/Tables/User.cs
@@ -3,7 +3,8 @@
     public class User : SystemTableFields
     {
 
-   [Key]  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+   [Key] 
+ [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; }
 
         public required string UserName { get; set; }
